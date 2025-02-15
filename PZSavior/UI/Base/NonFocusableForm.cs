10f263ec3ec0e3823aba0362e0cebcd02fd6687e
Petr Vosoust <petr.vosoust.ml@email.cz>
2025-02-15 04:13:09 +0100

    Fixed issue
    - unvisible wait dialog is no longer clickable

---
+++ PZSavior/UI/Base/NonFocusableForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PZSavior.UI.Base
+{
+
+    public class NonFocusableForm : Form
+    {
+        [DllImport("user32.dll")]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        private const int GWL_EXSTYLE = -20;
+        private const int WS_EX_LAYERED = 0x80000;
+        private const int WS_EX_TRANSPARENT = 0x20;
+
+        public NonFocusableForm()
+        {
+            int extendedStyle = GetWindowLong(this.Handle, GWL_EXSTYLE);
+            SetWindowLong(this.Handle, GWL_EXSTYLE, extendedStyle | WS_EX_LAYERED | WS_EX_TRANSPARENT);
+        }
+
+    }
+}
